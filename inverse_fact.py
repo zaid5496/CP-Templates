473f69f926210a9951d105df65e358838c109982
zaid5496 <150929608+zaid5496@users.noreply.github.com>
2025-03-28 16:19:47 +0530

    Update inverse_fact.py

--- inverse_fact.py
+++ inverse_fact.py
@@ -19,13 +19,18 @@ def C(n, r):
         return 0
     return (fact[n] * inv[r] % MOD) * inv[n - r] % MOD
 
+def P(n, r):
+    """Computes the permutation count P(n, r) = nPr modulo MOD."""
+    if r > n or r < 0:
+        return 0
+    return (fact[n] * inv[n - r]) % MOD
 
 fact, inv = precompute_factorials(MAX, MOD)
 
 
 print(C(10, 3))  # Output: 120
 print(C(5, 2))   # Output: 10
-
+print(P(5, 2))   # Output: 20
     
 # when we take modulo by a MOD the remainder is the answer eg (a%MOD),
 # but when the items are in denomenator as well it is not possible so we invert them,
